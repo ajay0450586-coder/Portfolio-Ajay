149a6c2306ea477936917036eca473635594a244
Ajay Kumar <ajay0450586@gmail.com>
2026-07-03 20:03:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+sections.forEach((section) => {
+  section.style.opacity = "0";
+  section.style.transform = "translateY(50px)";
+  section.style.transition = "1s";
+
+  observer.observe(section);
+});
